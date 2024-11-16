18df3ef94b3f755cd952c1e3dbefb6e60a52ba6c
Ivan Pakhomov <ip1133@ya.ru>
2024-11-16 13:45:11 +0300

    feat: second task solved

--- test.js
+++ test.js
@@ -13,3 +13,24 @@ const isPalindrome = (str) => {
 
 // console.log(isPalindrome('Привет'))
 // console.log(isPalindrome('А роза упала на лапу Азора'))
+
+// Task 2:
+
+const fizzBuzz = () => {
+  let count = 0
+
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (count % 3 === 0) {
+      console.log('Fizz')
+    } else if (count % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(count)
+    }
+    count += 1
+  }
+}
+
+// fizzBuzz()
