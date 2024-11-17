75c28ac3448dc7a257c389b18c6a357d42a486a4
Ivan Pakhomov <ip1133@ya.ru>
2024-11-17 23:19:47 +0300

    feat: task 5.2 solved

---
+++ Task_5/Task_5.2_ObjectClone/index.js
@@ -0,0 +1,20 @@
+const deepClone = (obj) => {
+  // return { ...obj } // При таком пкопировании, сохранятся ссылка на вложенные объекты.
+  // return Object.assign({}, obj) // При таком, аналогично.
+  return JSON.parse(JSON.stringify(obj)) // Копирует в том числе вложенные объекты.
+}
+
+const original = {
+  name: 'John',
+  address: {
+    city: 'New York',
+    country: 'USA',
+  },
+}
+
+const copy = deepClone(original)
+
+copy.address.city = 'Los Angeles'
+
+console.log(original.address.city) // New York (оригинальный объект не должен измениться)
+console.log(copy.address.city) // Los Angeles
