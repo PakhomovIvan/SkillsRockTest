9f166cabd2954f55d5cd2465e3dad5e4241ffcc0
Ivan Pakhomov <ip1133@ya.ru>
2024-11-17 16:25:36 +0300

    feat: third task solved

--- test.js
+++ test.js
@@ -11,8 +11,8 @@ const isPalindrome = (str) => {
   return strTrim === strRev
 }
 
-// console.log(isPalindrome('Привет'))
-// console.log(isPalindrome('А роза упала на лапу Азора'))
+console.log(isPalindrome('Привет'))
+console.log(isPalindrome('А роза упала на лапу Азора'))
 
 // Task 2:
 
@@ -33,4 +33,19 @@ const fizzBuzz = () => {
   }
 }
 
-// fizzBuzz()
+fizzBuzz()
+
+// Task 3:
+
+const chunkArray = (arr, size) => {
+  const resArr = []
+
+  for (let i = 0; i < Math.ceil(arr.length / size); i += 1) {
+    resArr[i] = arr.slice(i * size, i * size + size)
+  }
+
+  return resArr
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
